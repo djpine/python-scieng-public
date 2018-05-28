c1a21482c3a6a823e2a7c4a62dd8e1e57a846dfb
djpine <pine@nyu.edu>
2018-05-28 17:26:53 -0400

    program

---
+++ chap08/FitSpectrum1.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec  # unequal plots
+import scipy.optimize
+
+
+# define fitting function
+def GaussPolyBase(f, a, b, c, P, fp, fw):
+    return a+b*f+c*f*f+P*np.exp(-0.5*((f-fp)/fw)**2)
+
+# read in spectrum from data file
+# f=frequency, s=signal, ds=s uncertainty
+f, s, ds = np.loadtxt("Spectrum.txt", skiprows=4,
+                      unpack=True)
+
+# initial guesses for fitting parameters
+a0, b0, c0 = 60., -3., 0.
+P0, fp0, fw0 = 80., 11., 2.
+
+# fit data using SciPy's Levenberg Marquart method
+nlfit, nlpcov = scipy.optimize.curve_fit(GaussPolyBase,
+         f, s, p0=[a0, b0, c0, P0, fp0, fw0], sigma=ds)
+
+# unpack fitting parameters
+a, b, c, P, fp, fw = nlfit
+# unpack uncertainties in fitting parameters from
+# diagonal of covariance matrix
+da, db, dc, dP, dfp, dfw = [np.sqrt(nlpcov[j, j])
+                            for j in range(nlfit.size)]
+
+# create fitting function from fitted parameters
+f_fit = np.linspace(0.0, 25., 128)
+s_fit = GaussPolyBase(f_fit, a, b, c, P, fp, fw)
+
+# Calculate residuals and reduced chi squared
+resids = s - GaussPolyBase(f, a, b, c, P, fp, fw)
+redchisqr = ((resids/ds)**2).sum()/float(f.size-6)
+
+# Create figure window to plot data
+fig = plt.figure(1, figsize=(9.5, 6.5))
+gs = gridspec.GridSpec(2, 1, height_ratios=[6, 2])
+
+# Top plot: data and fit
+ax1 = fig.add_subplot(gs[0])
+ax1.plot(f_fit, s_fit, '-C0')
+ax1.errorbar(f, s, yerr=ds, fmt='oC3', ecolor='black')
+ax1.set_xlabel('frequency (THz)')
+ax1.set_ylabel('absorption (arb units)')
+ax1.text(0.7, 0.95, 'a = {0:0.1f}$\pm${1:0.1f}'
+         .format(a, da), transform=ax1.transAxes)
+ax1.text(0.7, 0.90, 'b = {0:0.2f}$\pm${1:0.2f}'
+         .format(b, db), transform=ax1.transAxes)
+ax1.text(0.7, 0.85, 'c = {0:0.2f}$\pm${1:0.2f}'
+         .format(c, dc), transform=ax1.transAxes)
+ax1.text(0.7, 0.80, 'P = {0:0.1f}$\pm${1:0.1f}'
+         .format(P, dP), transform=ax1.transAxes)
+ax1.text(0.7, 0.75, 'fp = {0:0.1f}$\pm${1:0.1f}'
+         .format(fp, dfp), transform=ax1.transAxes)
+ax1.text(0.7, 0.70, 'fw = {0:0.1f}$\pm${1:0.1f}'
+         .format(fw, dfw), transform=ax1.transAxes)
+ax1.text(0.7, 0.60, '$\chi_r^2$ = {0:0.2f}'
+         .format(redchisqr), transform=ax1.transAxes)
+ax1.set_title('$s(f)=a+bf+cf^2+P\,e^{-(f-f_p)^2/2f_w^2}$')
+
+# Bottom plot: residuals
+ax2 = fig.add_subplot(gs[1])
+ax2.errorbar(f, resids, yerr=ds, ecolor="black",
+             fmt="oC3")
+ax2.axhline(color="gray", zorder=-1)
+ax2.set_xlabel('frequency (THz)')
+ax2.set_ylabel('residuals')
+ax2.set_ylim(-20, 20)
+ax2.set_yticks((-20, 0, 20))
+
+plt.show()
+plt.savefig("FitSpectrum.pdf")
---
+++ chap08/betaDecay.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def LineFitWt(x, y, dy):
+    """
+    Fit to straight line.
+    Inputs: x, y, and dy (y-uncertainty) arrays.
+    Ouputs: slope and y-intercept of best fit to data.
+    """
+    dy2 = dy**2
+    norm = (1./dy2).sum()
+    xhat = (x/dy2).sum() / norm
+    yhat = (y/dy2).sum() / norm
+    slope = ((x-xhat)*y/dy2).sum()/((x-xhat)*x/dy2).sum()
+    yint = yhat - slope*xhat
+    dy2_slope = 1./((x-xhat)*x/dy2).sum()
+    dy2_yint = dy2_slope * (x*x/dy2).sum() / norm
+    return slope, yint, np.sqrt(dy2_slope), np.sqrt(dy2_yint)
+
+
+def redchisq(x, y, dy, slope, yint):
+    chisq = (((y-yint-slope*x)/dy)**2).sum()
+    return chisq/float(x.size-2)
+
+# Read data from data file
+t, N, dN = np.loadtxt("betaDecay.txt", skiprows=2, unpack=True)
+
+# Code to tranform & fit data starts here
+
+# Transform data and parameters to linear form: Y = A + B*X
+X = t          # transform t data for fitting (trivial)
+Y = np.log(N)  # transform N data for fitting
+dY = dN/N      # transform uncertainties for fitting
+
+# Fit transformed data X, Y, dY --> fitting parameters A & B
+# Also returns uncertainties in A and B
+B, A, dB, dA = LineFitWt(X, Y, dY)
+# Return reduced chi-squared
+redchisqr = redchisq(X, Y, dY, B, A)
+
+# Determine fitting parameters for exponential function
+# N = N0 exp(-t/tau) ...
+N0 = np.exp(A)
+tau = -1.0/B
+# ... and their uncertainties
+dN0 = N0 * dA
+dtau = tau**2 * dB
+
+# Code to plot transformed data and fit starts here
+
+# Create line corresponding to fit using fitting parameters
+# Only two points are needed to specify a straight line
+Xext = 0.05*(X.max()-X.min())
+Xfit = np.array([X.min()-Xext, X.max()+Xext])
+Yfit = A + B*Xfit
+
+plt.errorbar(X, Y, dY, fmt="oC0")
+plt.plot(Xfit, Yfit, "-C1", zorder=-1)
+plt.xlim(0, 100)
+plt.ylim(1.5, 7)
+plt.title(r"$\mathrm{Fit\ to:}\ \ln N = -t/\tau + \ln N_0$")
+plt.xlabel("$t$")
+plt.ylabel("ln($N$)")
+plt.text(50, 6.6, "A = ln N0 = {0:0.2f} $\pm$ {1:0.2f}"
+         .format(A, dA))
+plt.text(50, 6.3, "B = -1/tau = {0:0.4f} $\pm$ {1:0.4f}"
+         .format(-B, dB))
+plt.text(50, 6.0, "$\chi_r^2$ = {0:0.3f}"
+         .format(redchisqr))
+plt.text(50, 5.7, "N0 = {0:0.0f} $\pm$ {1:0.0f}"
+         .format(N0, dN0))
+plt.text(50, 5.4, "tau = {0:0.1f} $\pm$ {1:0.1f} days"
+         .format(tau, dtau))
+plt.show()
+plt.savefig("betaDecay.pdf")
