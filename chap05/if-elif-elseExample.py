67b56538a2c0214d45fe5faf422278e6c5fe9f4b
djpine <pine@nyu.edu>
2018-05-28 12:34:53 -0400

    first Chap 5 program

---
+++ chap05/if-elif-elseExample.py
@@ -0,0 +1,11 @@
+a = float(input("What is the coefficient a? "))
+b = float(input("What is the coefficient b? "))
+c = float(input("What is the coefficient c? "))
+d = b*b - 4.*a*c
+if d >= 0.0:
+    print("Solutions are real")       # block 1
+elif b == 0.0:
+    print("Solutions are imaginary")  # block 2
+else:
+    print("Solutions are complex")    # block 3
+print("Finished")
