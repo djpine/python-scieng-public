98db3636df949f98e5a4087922cf4a860b3306e3
djpine <pine@nyu.edu>
2018-05-28 11:04:49 -0400

    first file

---
+++ chap02/myTrip.py
@@ -0,0 +1,10 @@
+# Calculates time, gallons of gas used, and cost of
+# gasoline for a trip
+distance = 400.       # miles
+mpg = 30.             # car mileage
+speed = 60.           # average speed
+costPerGallon = 2.85  # price of gas
+
+time = distance/speed
+gallons = distance/mpg
+cost = gallons*costPerGallon
