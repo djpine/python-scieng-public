201db5ae5e247209d0da4c39e89032118d96f783
David Pine <pine@nyu.edu>
2019-07-04 14:15:33 -0400

    format changes

--- chap06/twoAxes.py
+++ chap06/twoAxes.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 fig, ax1 = plt.subplots(figsize=(7.5, 4.5))
 xa = np.linspace(0.01, 6.0, 150)
-ya = np.sin(np.pi*xa)/xa
+ya = np.sin(np.pi * xa) / xa
 ax1.plot(xa, ya, '-C0')
 ax1.set_xlabel('x (micrometers)')
 # Make y-axis label, ticks and numbers match line color.
@@ -12,14 +12,14 @@ ax1.tick_params('y', colors='C0')
 
 ax2 = ax1.twinx()  # use same x-axis for a 2nd (right) y-axis
 xb = np.arange(0.3, 6.0, 0.3)
-yb = np.exp(-xb*xb/9.0)
+yb = np.exp(-xb * xb / 9.0)
 ax2.plot(xb, yb, 'oC3')
 ax2.set_ylabel('decay', color='C3')  # axis label
-ax2.tick_params('y', colors='C3')    # ticks & numbers
+ax2.tick_params('y', colors='C3')  # ticks & numbers
 
 fig.tight_layout()
 fig.savefig('figures/twoAxes.pdf')
-fig.show()
+plt.show()
 
 """
 Introduction to Python for Science & Engineering
