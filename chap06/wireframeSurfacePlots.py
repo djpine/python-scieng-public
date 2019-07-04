5976b5d6760c9b1310a59cb0878b08bd860a3d9d
David Pine <pine@nyu.edu>
2019-07-04 14:21:20 -0400

    format changes

--- chap06/wireframeSurfacePlots.py
+++ chap06/wireframeSurfacePlots.py
@@ -4,9 +4,9 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def pmgauss(x, y):
-    r1 = (x-1)**2 + (y-2)**2
-    r2 = (x-3)**2 + (y-1)**2
-    return 2*np.exp(-0.5*r1) - 3*np.exp(-2*r2)
+    r1 = (x - 1) ** 2 + (y - 2) ** 2
+    r2 = (x - 3) ** 2 + (y - 1) ** 2
+    return 2 * np.exp(-0.5 * r1) - 3 * np.exp(-2 * r2)
 
 
 a, b = 4, 3
@@ -21,8 +21,8 @@ fig, ax = plt.subplots(1, 2, figsize=(9.2, 4),
                        subplot_kw={'projection': '3d'})
 for i in range(2):
     ax[i].set_zlim(-3, 2)
-    ax[i].xaxis.set_ticks(range(a+1))  # manually set ticks
-    ax[i].yaxis.set_ticks(range(b+1))
+    ax[i].xaxis.set_ticks(range(a + 1))  # manually set ticks
+    ax[i].yaxis.set_ticks(range(b + 1))
     ax[i].set_xlabel(r'$x$')
     ax[i].set_ylabel(r'$y$')
     ax[i].set_zlabel(r'$f(x,y)$')
@@ -37,4 +37,4 @@ p1 = ax[1].plot_surface(X, Y, Z, rcount=50, ccount=50,
                         color='C1')
 fig.subplots_adjust(left=0.0)
 fig.savefig('./figures/wireframeSurfacePlots.pdf')
-fig.show()
+plt.show()
