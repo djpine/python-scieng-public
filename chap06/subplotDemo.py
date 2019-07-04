1000fefdc465c465590023800e1e31f952752a83
David Pine <pine@nyu.edu>
2019-07-04 14:14:13 -0400

    format changes

--- chap06/subplotDemo.py
+++ chap06/subplotDemo.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 theta = np.arange(0.01, 8., 0.04)
-y = np.sqrt((8./theta)**2-1.)
+y = np.sqrt((8. / theta) ** 2 - 1.)
 ytan = np.tan(theta)
 ytan = np.ma.masked_where(np.abs(ytan) > 20., ytan)
-ycot = 1./np.tan(theta)
+ycot = 1. / np.tan(theta)
 ycot = np.ma.masked_where(np.abs(ycot) > 20., ycot)
 
 plt.figure(figsize=(8.5, 6))
@@ -16,11 +16,11 @@ plt.plot(theta, ytan)
 plt.xlim(0, 8)
 plt.ylim(-8, 8)
 plt.axhline(color="gray", zorder=-1)
-plt.axvline(x=np.pi/2., color="gray", linestyle='--',
+plt.axvline(x=np.pi / 2., color="gray", linestyle='--',
             zorder=-1)
-plt.axvline(x=3.*np.pi/2., color="gray", linestyle='--',
+plt.axvline(x=3. * np.pi / 2., color="gray", linestyle='--',
             zorder=-1)
-plt.axvline(x=5.*np.pi/2., color="gray", linestyle='--',
+plt.axvline(x=5. * np.pi / 2., color="gray", linestyle='--',
             zorder=-1)
 plt.xlabel("theta")
 plt.ylabel("tan(theta)")
@@ -33,7 +33,7 @@ plt.ylim(-8, 8)
 plt.axhline(color="gray", zorder=-1)
 plt.axvline(x=np.pi, color="gray", linestyle='--',
             zorder=-1)
-plt.axvline(x=2.*np.pi, color="gray", linestyle='--',
+plt.axvline(x=2. * np.pi, color="gray", linestyle='--',
             zorder=-1)
 plt.xlabel("theta")
 plt.ylabel("cot(theta)")
