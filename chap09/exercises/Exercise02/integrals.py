4ecae3a30a757193c4c83d62ea7ef8afd995d55c
David Pine <pine@nyu.edu>
2019-11-16 21:29:40 -0500

    Reformated

--- chap09/exercises/Exercise02/integrals.py
+++ chap09/exercises/Exercise02/integrals.py
@@ -1,12 +1,12 @@
 import numpy as np
-from scipy.integrate import quad
+import scipy.integrate
 
 
 def f(x):
     return 1.0 / (1 + x * x)
 
 
-i1, er1 = quad(f, -1, 1)
+i1, er1 = scipy.integrate.quad(f, -1, 1)
 i1exact = np.pi / 2.
 print('\n*************************************************')
 print('\nI1 = {} \u00B1 {}'.format(i1, er1))
@@ -18,7 +18,7 @@ def g(x):
     return 1.0 / (a * a + np.pi ** 2)
 
 
-i2, er2 = quad(g, -np.inf, np.inf)
+i2, er2 = scipy.integrate.quad(g, -np.inf, np.inf)
 i2exact = 2. / 3.
 print('\nI2 = {} \u00B1 {}'.format(i2, er2))
 print('  relative error {}'.format((i2 - i2exact) / i2exact))
